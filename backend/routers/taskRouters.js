07e35635c322c8e152356c57825e07d0c4588757
Zep213 <jososephbem100@gmail.com>
2025-10-28 19:50:44 -0300

    hotfix: routers

--- backend/routers/taskRouters.js
+++ backend/routers/taskRouters.js
@@ -6,5 +6,6 @@ router.post('/tasks', taskController.createTask);
 router.get('/tasks', taskController.getTasks);
 router.put('/tasks/:id', taskController.updateTask);
 router.delete('/tasks/:id', taskController.deleteTask);
+router.patch('/tasks/atualizarPosicao', taskController.atualizarPosicao);
 
 module.exports = router;
