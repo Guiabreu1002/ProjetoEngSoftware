5696d243dd40b156d6a2cb7ef1f5497d9a8bf79d
Zep213 <jososephbem100@gmail.com>
2025-10-28 19:16:01 -0300

    feat nemetags

--- backend/schema/taskSchema.js
+++ backend/schema/taskSchema.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-    title: { type: String, required: true},
-    description: { type: String, required: true},
-    status: { type: String, enum: ['Pendente', 'Em Progresso', 'Completa'], default: 'Pendente'},
-    dueDate: { type: Date, required: true}
+    titulo: { type: String, required: true},
+    descricao: { type: String, required: true},
+    ordem: 0,
+    etiqueta: {type: mongoose.Schema.Types.ObjectId, ref: 'Etiquetas' },
+    dueDate: { type: Date, required: true},
+    dono: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    membros: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
     });
 
 module.exports = mongoose.model('Task', taskSchema);
